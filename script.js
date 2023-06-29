a65f6ebd50e9bc0d163a2ae0a970e0c2dc157af6
Mukul Vashishth <mukulvashishth8@gmail.com>
2023-06-29 22:56:48 +0530

    final commit UI and sorted functionality

--- script.js
+++ script.js
@@ -2,15 +2,25 @@ const userNameInput = document.getElementById("userName");
 const showDetailsButton = document.getElementById("showDetails");
 const profileInfoDiv = document.getElementById("profileInfo");
 const repoInfoDiv = document.getElementById('reposInfo');
+const sortContainer = document.querySelector(".sortBox");
 
 showDetailsButton.addEventListener('click', async () => {
     const userName = userNameInput.value;
 
     //request data from Server : fetch API provides an interface for fetching resources
-    const res = await fetch(`https://api.github.com/users/${userName}`)
+        const res = await fetch(`https://api.github.com/users/${userName}`)
         const data = await res.json(); 
-        showProfile(data);
-        showRepoInfo(userName);
+        //Adding a validation if profile exists or not
+        if(res.status == 404) {
+            profileInfoDiv.innerHTML = "No profile exists";
+            sortContainer.style.display = "none";
+            repoInfoDiv.innerHTML = '';
+        }
+        else {
+            sortContainer.style.display = "block";
+            showProfile(data);
+            showRepoInfo(userName);
+        }
 })
 
 function showProfile(data) {
@@ -24,7 +34,7 @@ function showProfile(data) {
             <div class="card-subHeading">${data.login}</div>
             <div class="card-text">
                 <p>${data.bio}</p>
-                <p>${data.followers} followers &nbsp; ${data.following} following
+                <p>${data.followers} followers &nbsp; ${data.following} following </p>
             </div>        
         </div>
         </div>
@@ -41,16 +51,51 @@ async function showRepoInfo(userName) {
     const res = await fetch(`https://api.github.com/users/${userName}/repos`)
     const projects = await res.json();
 
-        console.log(projects);
-        for(let i=0;i<projects.length;i++) {
+        // console.log(projects);
+    const sortProjects = projects.sort((a,b) => b.stargazers_count - a.stargazers_count);
+    showProjects(sortProjects);
+
+    const options = document.getElementById("options");
+    options.addEventListener("change", () => {
+        const selectedOption = options.value;
+        console.log(selectedOption);
+        let sortProjects;
+        if(selectedOption === "stars") {
+            sortProjects = projects.sort((a,b) => b.stargazers_count - a.stargazers_count);
+        }
+        else if(selectedOption === "forks") {
+            sortProjects = projects.sort((a,b) => b.forks - a.forks);
+        }
+        else if(selectedOption === "size") {
+            sortProjects = projects.sort((a,b) => b.size - a.size);
+        }
+        showProjects(sortProjects);
+    });
+
+    function showProjects(sortProjects) {
+        repoInfoDiv.innerHTML = "";
+
+        for(let i=0;i<sortProjects.length;i++) {
             repoInfoDiv.innerHTML += `
             <div class="card">  
             <div class="card-body">
-                <div class="card-title">${projects[i].name}</div>
-                <div class="card-subHeading">${projects[i].language}</div>
+                <div class="card-title">${sortProjects[i].name}</div>
+                <div class="card-description">${sortProjects[i].description}</div>
+                <div class="card-subHeading"> <span>${
+                    sortProjects[i].language
+                  }</span>
+                  <span> <i class="fa-solid fa-code-fork"></i> ${
+                    sortProjects[i].forks
+                  }</span>
+                  <span> <i class="fa-solid fa-star"></i> ${
+                    sortProjects[i].stargazers_count
+                  }</span>
+                  <span>${sortProjects[i].size} Kb</span>
+                  </div>
+                
                 <div class="card-text">
                     <button>
-                        <a href=${projects[i].html_url}>
+                        <a href=${sortProjects[i].html_url}>
                             Do checkout the project
                         </a>
                     </button>
@@ -58,7 +103,9 @@ async function showRepoInfo(userName) {
             </div>
             </div>        
             `
-        } 
+        }
+    }
+    showProjects(projects);   
 }
 
 
